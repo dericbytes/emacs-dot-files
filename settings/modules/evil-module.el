af18b110b5e815a8c081048bc08032e2bad4feac
dericbytes <dericbytes@nowhere.com>
2020-12-23 18:53:21 +0000

    evil hide mode change feedback

--- settings/modules/evil-module.el
+++ settings/modules/evil-module.el
@@ -7,4 +7,17 @@
 (setq evil-escape-delay 0.20)
 (evil-escape-mode)
 
+
+(setq evil-visual-state-message "")
+(setq evil-insert-state-message "")
+(setq evil-visual-block-message "B")
+
+
+(setq evil-visual-state-tag "")
+(setq evil-insert-state-tag "")
+(setq evil-emacs-state-tag "")
+(setq evil-normal-state-cursor '(box "orange"))
+(setq evil-emacs-state-cursor '((bar . 3) "magenta")) 
+(setq evil-insert-state-cursor '((bar . 3) "magenta")) 
+
 (provide 'evil-module)
