d5147373a13c1314c882b247daa248e1aac333de
dericbytes <dericbytes@nowhere.com>
2020-12-23 18:56:21 +0000

    add util fns - color emoji support + yasnippet

--- init.el
+++ init.el
@@ -6,16 +6,30 @@
 
 (add-to-list 'load-path (expand-file-name "settings" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "./settings/modules" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "./settings/packages" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "./settings/overwrite" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "./settings/utils" user-emacs-directory))
 (require 'init-general)
 
 (require 'mode-line-module)
 (require 'evil-module)
+
+(require 'init-yasnippet)
+(require 'overwrite-read-extended-command-change-Mx)
+(require 'figlet-utils)
+(require 'restart-emacs-utils)
+(require 'screencast-utils)
+
+;; enable color emojis
+(set-fontset-font t 'symbol "Noto Color Emoji" nil 'append)
 
 ;; it is no secret emacs 
 (require 'epa-file)
 (epa-file-enable)
 (load (expand-file-name "secret.el.gpg" user-emacs-directory))
 ;; (load "~/wd/emacs-dot-files/secret.el.gpg")
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
