a09cd53e4f03822fbdd8e6502572f146af4bd530
dericbytes <dericbytes@nowhere.com>
2020-12-24 14:08:00 +0000

    screencast initializacion ya tiene su propio archivo

--- init-screencast.el
+++ init-screencast.el
@@ -1,3 +1,30 @@
-(load "init.el)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+
+(fset 'display-startup-echo-area-message 'ignore)
+(setq inhibit-startup-screen t)
+;; (eval '(setq inhibit-startup-echo-area-message "Deric Bytes"))
+
+;; (defun display-startup-echo-area-message ()
+;;   (message "Let the hacking begin!"))
+
+
+
+;; (advice-add #'display-startup-echo-area-message :override #'ignore)
+;; (find-file "~/tmp/test.c")
+;; ;; (advice-add #'display-startup-echo-area-message :override #'ignore)
+;; (defun display-startup-echo-area-message ()
+;;   (message "hi"))
+
+(setq echo-keystrokes 0) 
+(setq inhibit-message t)
+
+(setq user-emacs-directory "/home/dericbytes/wd/emacs-dot-files/")
+(load-file (expand-file-name "init.el" user-emacs-directory))
+(setq SCREENCAST t)
 
 (require 'screencast-utils)
+(screencast)
+(message "")
+
