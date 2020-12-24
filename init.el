a3880103b9ad8c396668b6f75d24c4940bdf98fa
dericbytes <dericbytes@nowhere.com>
2020-12-24 14:05:40 +0000

    load general emacs setting before cask init

--- init.el
+++ init.el
@@ -1,15 +1,16 @@
 (setq user-emacs-directory "/home/dericbytes/wd/emacs-dot-files/")
 (setq user-init-file (expand-file-name "/init.el" user-emacs-directory))
 
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-
 (add-to-list 'load-path (expand-file-name "settings" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "./settings/modules" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "./settings/packages" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "./settings/overwrite" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "./settings/utils" user-emacs-directory))
+
 (require 'init-general)
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
 
 (require 'mode-line-module)
 (require 'evil-module)
