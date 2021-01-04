81fdef1a913dd43a7961183bee403391cb6d6a3e
dericbytes <dericbytes@nowhere.com>
2021-01-04 01:42:15 +0000

    add global-auto-revert-mode org-confirm-babel-evaluate

--- init-screencast.el
+++ init-screencast.el
@@ -3,6 +3,9 @@
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 
+;; reload files if changed on disk
+(global-auto-revert-mode t)
+
 (fset 'display-startup-echo-area-message 'ignore)
 (advice-add #'display-startup-echo-area-message :override #'ignore)
 
@@ -16,7 +19,9 @@
 
 (add-to-list 'load-path "~/emacs-config/dem/utils-dem/")
 (add-to-list 'load-path "~/emacs-config/dem/libs-dem/")
-(require 'slide-show-dem)
+
+(require 'slides-show-dem)
+(require 'org-hide-src-block-delimiters-mode)
 (require 'compilation-ovl-hide-mode)
 (compilation-ovl-hide)
 
@@ -31,16 +36,22 @@
   (set-window-buffer nil (current-buffer)))
 
 (defun set-mode-line-falla-conmigo ()
-  (cond
-    ((comint-check-proc (current-buffer))
-     (setq mode-line-format " Shell "))
-    ((eq major-mode 'dired-mode)
-     (setq mode-line-format " File Manager "))
-    (t
-     (setq mode-line-format " ")  )))
+  (when custom1-mode-line 
+    (cond
+      ((comint-check-proc (current-buffer))
+       (setq mode-line-format " Shell "))
+      ((eq major-mode 'dired-mode)
+       (setq mode-line-format " File Manager "))
+      (t
+       (setq mode-line-format " Falla Conmigo")  ))
+    (force-mode-line-update)))
 
 (add-hook 'after-change-major-mode-hook 'set-mode-line-falla-conmigo)
 
 
-(slides-start-show--falla-conmigo-pageN)
+(slides-start-show--falla-conmigo-slideN)
+
+(setq org-confirm-babel-evaluate nil)
+
+
 (setq compile-command "")
